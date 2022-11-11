f8a9c65ac735f9d85ec639be3af2055b6bfa2512
G Codes <graciela.mejia1@yahoo.com>
2022-11-11 14:08:54 -0800

    start button working/countdown counting

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+var countDown = 60;
+var startBtn = document.getElementById("start-btn");
+var timer = document.querySelector(".timer");
+var questions = document.querySelector(".questions");
+
+// //timer on landing page
+// timer.textContent = countDown;
+// countDown--;
+
+//start button
+startBtn.addEventListener("click", startGame);
+
+function startGame() {
+  // Start timer
+  var tickTock = setInterval(() => {
+    timer.textContent = countDown;
+    countDown--;
+
+    if (countDown < 0) {
+      clearInterval(tickTock);
+      // endGame()
+    }
+  }, 1000);
+
+  timer.classList.remove("hidden");
+  questions.classList.remove("hidden");
+}
+
+var questions = [
+  {
+    question: "What is a boolean?",
+    choices: ["a string", "multiple choice", "two values"],
+    answer: "two values",
+  },
+  {
+    question: "",
+  },
+];
